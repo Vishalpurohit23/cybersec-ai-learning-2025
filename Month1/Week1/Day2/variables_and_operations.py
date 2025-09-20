4e7034c6e7f68c226a7a706bd6343baa37fd797d
Vishalpurohit23 <vishal.purohit23@gmail.com>
2025-09-20 14:04:37 +0530

    Update variables_and_operations.py
    File: variables_and_operations.py

--- Month1/Week1/Day2/variables_and_operations.py
+++ Month1/Week1/Day2/variables_and_operations.py
@@ -1,1 +1,33 @@
+# variables_and_operations.py
+
+# Integer variable
+age = 15
+print("Age:", age)  # Output: Age: 15
+
+# Float variable
+height = 1.75
+print("Height:", height)  # Output: Height: 1.75
+
+# String variable
+name = "Alex"
+print("Name:", name)  # Output: Name: Alex
+
+# Boolean variable
+is_student = True
+print("Is student?", is_student)  # Output: Is student? True
+
+# Check data types
+print(type(age))      # Output: <class 'int'>
+print(type(height))   # Output: <class 'float'>
+print(type(name))     # Output: <class 'str'>
+print(type(is_student)) # Output: <class 'bool'>
+
+# Arithmetic operations
+math_score = 95
+science_score = 88
+total = math_score + science_score   # Addition
+average = total / 2                 # Division
+
+print("Total Score:", total)        # Output: 183
+print("Average Score:", average)    # Output: 91.5
 
